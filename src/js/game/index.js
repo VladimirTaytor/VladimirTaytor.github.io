a3d615cf96265a2057821eb7481f240ad1a6a0c8
roman-sereda <lonely.phobos@gmail.com>
2018-01-27 15:25:46 +0200

    Add camera moving

--- src/js/game/index.js
+++ src/js/game/index.js
@@ -4,8 +4,8 @@ class Game{
 
   constructor(){
     this.game = new Phaser.Game(
-      800,
-      600,
+      800 /*window.innerWidth*/,
+      600 /*window.innerHeight*/,
       Phaser.AUTO,
       'peluh_loh', { preload: this.preload, create: this.create, update: this.update });
   }
@@ -17,25 +17,38 @@ class Game{
   create(){
     this.player = this.game.add.sprite(100, 100, 'pukich_backward');
 
+    this.game.world.setBounds(0, 0, 7680, 5120);
+    this.game.physics.startSystem(Phaser.Physics.P2JS);
+    this.game.physics.p2.enable(this.player);
+    this.player.body.fixedRotation = true;
+
+    this.cursors = this.game.input.keyboard.createCursorKeys();
+
+    this.game.camera.follow(this.player, Phaser.Camera.FOLLOW_LOCKON, 0.1, 0.1);
+
+    //sprites
+
     this.player.animations.add('walk');
     this.player.animations.play('walk', 4, true);
   }
 
   update(){
 
-    if(this.game.input.keyboard.isDown(Phaser.Keyboard.LEFT)){
-        this.player.x -= 4;
-    }
-    else if(this.game.input.keyboard.isDown(Phaser.Keyboard.RIGHT)){
-        this.player.x += 4;
-    }
+  this.player.body.setZeroVelocity();
 
-    if(this.game.input.keyboard.isDown(Phaser.Keyboard.UP)){
-        this.player.y -= 4;
-    }
-    else if(this.game.input.keyboard.isDown(Phaser.Keyboard.DOWN)){
-        this.player.y += 4;
-    }
+   if (this.cursors.up.isDown){
+       this.player.body.moveUp(300)
+   }
+   else if (this.cursors.down.isDown){
+       this.player.body.moveDown(300);
+   }
+
+   if (this.cursors.left.isDown){
+       this.player.body.velocity.x = -300;
+   }
+   else if (this.cursors.right.isDown){
+       this.player.body.moveRight(300);
+   }
   }
 
   start(){
