0490864be3354e2f0642bf96ee9aa086a8bc1742
roman-sereda <lonely.phobos@gmail.com>
2018-01-27 14:46:52 +0200

    Add controls to player

--- src/js/game/index.js
+++ src/js/game/index.js
@@ -7,18 +7,35 @@ class Game{
       800,
       600,
       Phaser.AUTO,
-      'peluh_loh', { preload: this.preload, create: this.create });
+      'peluh_loh', { preload: this.preload, create: this.create, update: this.update });
   }
 
   preload(){
-    this.game.load.spritesheet('pukich_forward', path + 'pukich/forward.png', 128, 128, 3);
+    this.game.load.spritesheet('pukich_backward', path + 'pukich/backward.png', 128, 128, 3);
   }
 
   create(){
     this.player = this.game.add.sprite(100, 100, 'pukich_backward');
 
     this.player.animations.add('walk');
-    this.player.animations.play('walk', 4, false);
+    this.player.animations.play('walk', 4, true);
+  }
+
+  update(){
+
+    if(this.game.input.keyboard.isDown(Phaser.Keyboard.LEFT)){
+        this.player.x -= 4;
+    }
+    else if(this.game.input.keyboard.isDown(Phaser.Keyboard.RIGHT)){
+        this.player.x += 4;
+    }
+
+    if(this.game.input.keyboard.isDown(Phaser.Keyboard.UP)){
+        this.player.y -= 4;
+    }
+    else if(this.game.input.keyboard.isDown(Phaser.Keyboard.DOWN)){
+        this.player.y += 4;
+    }
   }
 
   start(){
