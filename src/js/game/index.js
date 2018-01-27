ce277f08d50bb745f46adadea1f9384e65a2df3e
roman-sereda <wurger.wlf@gmail.com>
2018-01-27 12:50:08 +0200

    Add webgl window

--- src/js/game/index.js
+++ src/js/game/index.js
@@ -1,4 +1,28 @@
 class Game{
+
+  constructor(){
+    this.game = new Phaser.Game(
+      800,
+      600,
+      Phaser.AUTO,
+      'peluh_loh', { preload: this.preload, create: this.create, update: this.update });
+  }
+
+  preload(){
+
+  }
+
+  create(){
+
+  }
+
+  animationStarted(sprite, animation) {
+  }
+
+  upload(){
+
+  }
+
   start(){
     console.log('fuck you');
   }
